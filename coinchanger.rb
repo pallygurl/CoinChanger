f8f797e737dc8c1bc2b25ad906fef38e7dc7f9dc
Cyndi Bolton <cbolton@wvtpg.com>
2016-09-18 12:13:22 -0400

    test for 1 penny equals 1

--- coinchanger.rb
+++ coinchanger.rb
@@ -1,5 +1,22 @@
+=begin
+Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
+
+eg. if you have 11 cents you should return {:dime => 1, :penny => 1}; if you have 31 cents you should return {:quarter => 1, :nickel => 1, :penny => 1}.
+
+Build up your coin_changer using tests. Commit to git after every test.
+=end
+
+
 def coinchanger(coins)
 	moolah = {}
 	
-end	
+	  money = {:dollar => 100, :half_dollar => 50, :quarter => 25, :dime => 10, :nickle => 5, :penny => 1}
 
+	  	money.each do |coin, till|
+	  		if coins >= till
+	  			moolah[coin] = coins / till
+	  		end
+	  	end
+	  		
+	moolah
+end
--- test_coin_changer.rb
+++ test_coin_changer.rb
@@ -3,17 +3,14 @@ require_relative "coinchanger.rb"
 
 class TestCoinChanger <Minitest::Test
 
-	def test_if_you_aint_got_no_money_take_your_broke_ass_home
+	def test_if_you_aint_got_no_money_take_your_broke_ass_home	#zero coins returns an empty hash
 		coins = 0
 		assert_equal({}, coinchanger(coins))
 	end	
+
+	def test_give_a_penny_get_a_penny							#1 penny returns a hash (moolah) with 1 penny
+		coins = 1
+		assert_equal({:penny => 1}, coinchanger(coins))
+	end
 end
-
-
-
-
-
-
-
-
 
