4277fd74685aea4523753998fe4527a211539f9c
Cyndi Bolton <cbolton@wvtpg.com>
2016-09-26 15:36:09 -0400

    added coinchanger app

--- coinchanger2.rb
+++ coinchanger2.rb
@@ -8,10 +8,9 @@ num = 100
 money.each do |coin, till|
 	  		if coins >= till
 	  			moolah[coin] = coins / till  #coins received divided by the amount defined in the hash
-	  		end
-	  		coins = coins % till			# coins received divided by the amount defined in the hash with remainder
-	  										# need this for penny/nickle to work properly
+	  	  		coins = coins % till			# coins received divided by the amount defined in the hash with remainder
+	  										# need this for odd amounts to work properly
 	  	end
-	  		
+end	  		
 	moolah
 end
